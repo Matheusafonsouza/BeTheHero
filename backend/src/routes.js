282fe05649d8bd562de4bd9d8cf32293b6c20782
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-22 19:41:18 -0300

    implementação do controller de sessão e login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,12 @@ const express = require('express')
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 const connection = require('./database/connection')
 
 const routes = express.Router()
+
+routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
