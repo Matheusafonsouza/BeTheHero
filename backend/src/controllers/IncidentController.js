51fc577e4f41a71ff189cddb4c596be86ac36f15
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-22 18:38:07 -0300

    criação do controller de casos, criação de casos e listagem de casos

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,24 @@
+const connection = require('../database/connection')
+
+module.exports = {
+
+    async index(req,res){
+        const incidents = await connection('incidents').select('*')
+
+        return res.json(incidents)
+    },
+
+    async create(req,res){
+        const { title, description, value } = req.body
+        const ong_id = req.headers.authorization
+
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id
+        })
+
+        return res.json({id})
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,31 +1,14 @@
 const express = require('express')
-const crypto = require('crypto')
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
 const connection = require('./database/connection')
 
 const routes = express.Router()
 
-routes.get('/ongs', async (req,res)=>{
-    const ongs = await connection('ongs').select('*')
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
-    return res.json(ongs)
-})
+routes.post('/incidents', IncidentController.create)
+routes.get('/incidents', IncidentController.index)
 
-routes.post('/ongs', async (req,res)=>{
-    const {name,email,whatsapp,city,uf} = req.body
-
-    const id = crypto.randomBytes(4).toString('HEX')
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    })
-
-    return res.json({id})
-})
-
-
-module.exports = routes
+module.exports = routes  
