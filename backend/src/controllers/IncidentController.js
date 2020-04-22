0198030104417f0a5e4ac196ba5da178668577ff
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-22 19:59:52 -0300

    retornando dados da ong para o front

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -8,9 +8,10 @@ module.exports = {
         const [count] = await connection('incidents').count()
 
         const incidents = await connection('incidents')
+            .join('ongs','ongs.id','=','incidents.ong_id')
             .limit(5)
             .offset((page - 1 ) * 5)
-            .select('*')
+            .select(['incidents.*','ongs.name','ongs.email','ongs.whatsapp','ongs.city','ongs.uf'])
 
         res.header('X-Total-Count',count['count(*)'])
 
