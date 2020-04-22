4db373b8768d643e809da439dfd18c489084a0b6
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-22 19:35:50 -0300

    implementação do controller de profile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 const connection = require('./database/connection')
 
 const routes = express.Router()
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
+
+routes.get('/profile',ProfileController.index)
 
 routes.post('/incidents', IncidentController.create)
 routes.get('/incidents', IncidentController.index)
