fca935103c789511894fd88adbf02d2f2dfad590
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-22 15:15:41 -0300

    criando rotas para criação e listagem de ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const crypto = require('crypto')
+const connection = require('./database/connection')
 
 const routes = express.Router()
 
-routes.get('/users', (req,res)=>{
-    return res.json({
-        evento: 'Semana OmniStack',
-        aluno: 'Matheus Afonso'
+routes.get('/ongs', async (req,res)=>{
+    const ongs = await connection('ongs').select('*')
+
+    return res.json(ongs)
+})
+
+routes.post('/ongs', async (req,res)=>{
+    const {name,email,whatsapp,city,uf} = req.body
+
+    const id = crypto.randomBytes(4).toString('HEX')
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
     })
+
+    return res.json({id})
 })
 
 
