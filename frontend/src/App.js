069ff08ff5941a96a42262e7afd23538bffad4dd
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-23 11:27:00 -0300

    inserções de css e global.css e inicio da page logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from './Header'
+import Logon from './pages/Logon'
+import './global.css'
 
 function App() {
-  const [counter,setCounter] = useState(0)
-
-  
-
-  function increment(){
-    setCounter(counter+1)
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}> Incrementar </button>
-    </div>
+    <Logon/>
   );
 }
 
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import './styles.css'
+
 export default function Logon(){
     return(
-        <h1>Hello :-)</h1>
+        <div className="logon-container">
+            <section className="form">
+
+            </section>
+        </div>
     )
 }
