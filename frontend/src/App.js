251b0dd91ec0cad1703a1e9724de3c6bf8755cef
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-24 00:18:22 -0300

    criação das rotas de logon e register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Logon from './pages/Logon'
+import Routes from './routes'
 import './global.css'
 
 function App() {
   return (
-    <Logon/>
+    <Routes/>
   );
 }
 
