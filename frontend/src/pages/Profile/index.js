d2a34e66822cb91ac4b6bbe3fa8c210c922e11df
matheusafonsouza <matheusafonsouza@gmail.com>
2020-04-27 12:34:40 -0300

    estilização dos cards

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FiPower} from 'react-icons/fi'
+import {FiPower, FiTrash2} from 'react-icons/fi'
 
 import LogoImg from '../../assets/logo.svg'
 
@@ -18,5 +18,46 @@ export default function Profile(){
                     <FiPower size={18} color="#e02041"/>
                 </button>
             </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+                    <strong>VALOR:</strong>
+                    <p>120</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3"/>
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+                    <strong>VALOR:</strong>
+                    <p>120</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3"/>
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+                    <strong>VALOR:</strong>
+                    <p>120</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3"/>
+                    </button>
+                </li>
+            </ul>
         </div>
 )}
