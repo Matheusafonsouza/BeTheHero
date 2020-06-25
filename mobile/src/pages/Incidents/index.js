5207ae2cb9f56da226479605cae8ea19c4875dc4
matheusafonsouza <matheusafonsouza@gmail.com>
2020-06-25 01:49:18 -0300

    Adicionando real-time loading na página de incidentes

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,6 +12,9 @@ import styles from './styles';
 export default function Incident() {
     const [incidents, setIncidents] = useState([]);
     const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
     const navigation = useNavigation();
 
     function navigateToDetail(incident) {
@@ -19,9 +22,22 @@ export default function Incident() {
     }
 
     async function loadIncidents() {
-        const res = await api.get('/incidents');
-        setIncidents(res.data);
-        setTotal(res.headers['x-total-count'])
+        if (loading)
+            return
+        
+        if (total > 0 && incidents.length === total)
+            return
+
+        setLoading(true);
+
+        const res = await api.get('/incidents', {
+            params: { page }
+        });
+
+        setIncidents([...incidents, ...res.data]);
+        setTotal(res.headers['x-total-count']);
+        setPage(page+1);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -45,6 +61,8 @@ export default function Incident() {
                 style={styles.incidentList}
                 keyExtractor={incident => incident.id}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
